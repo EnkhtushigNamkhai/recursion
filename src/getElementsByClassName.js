65f124eb3f973e675b1216d788ebbabab93ccc7b
EnkhtushigNamkhai <enkhtushig@gmail.com>
2017-09-16 21:11:10 -0700

    Completed getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,27 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+
+  /*You should use document.body, element.childNodes, and element.classList
+	Returns an array-like object of all child elements which have all of the 
+	given class names. When called on the document object, the complete document 
+	is searched, including the root node. You may also call getElementsByClassName() 
+	on any element; it will return only elements which are descendants of the specified 
+	root element with the given class names.
+  */
+  //Returns the <body> or <frameset> node of the current document, or null if no such element exists.
+  var result = [];
+  var helper = function(node) {
+  	if (node.classList.contains(className)) {
+  		result.push(node);
+  	}
+  	
+  	var childrenList = node.children;
+  	for (var i = 0; i < childrenList.length; i++) {
+  		helper(childrenList[i]);
+  	}
+  	
+  }
+  helper(document.body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof obj === 'boolean') {
+  	return String(obj);
+  } else if (typeof obj === 'number') {
+  	return String(obj);
+  } else if (obj === null) {
+  	return 'null';
+  } else if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+
+  var result = [];
+  var stringify;
+  if (Array.isArray(obj)) {
+  	//if undefined in an Array, it becomes null
+  	for (var i = 0; i < obj.length; i++) {
+  		if (obj[i] === undefined || typeof obj[i] === 'function') {
+  			result.push('null');
+  		} else {
+  			result.push(stringifyJSON(obj[i]));
+  		}
+  	}
+  	stringify = "[" + result.join(",") + "]";
+  } else if (typeof obj === 'object') {
+  	//if the value of the key is undefined, skip over it
+  	for (var key in obj) {
+  		var keyValuePair = '"' + key + '"' + ":";
+  		var value = obj[key];
+  		if (typeof value === 'String') {
+  			keyValuePair += value;
+  		} else if (value === undefined || typeof value === 'function') {
+  			continue;
+  		} else {
+  			keyValuePair += stringifyJSON(value);
+  		}
+  		result.push(keyValuePair);
+  		//result will be a list of strings of key: value
+  	}
+  	stringify = "{" + result.join(",") + "}"
+  }
+  return stringify;
 };
